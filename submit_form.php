43b9d1d892fbd3406ada05b73c3977b71835c045
nabilqistubillah <bilqisbill02@gmail.com>
2024-11-11 20:57:01 +0700

    menambahkan code php

---
+++ submit_form.php
@@ -0,0 +1,11 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = htmlspecialchars($_post['name']);
+    $email = htmlspecialchars($_post['email']);
+    $message = htmlspecialchars($_post['message']);
+
+    echo "<h3>Terimakasih, $name!</h3>";
+    echo "<p>Email, $email</p>";
+    echo "<p>Pesan anda, $message</p>";
+}
+?>
